01136fbcb52f58bd57cc455ce07ebc1474fd546a
malariuz-ab <sb-101@yandex.ru>
2020-04-22 18:14:28 +0700

    Loadish is set up

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", '1');
+ini_set('error_reporting', '-1');
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>Getting Started</title>
+    </head>
+    <body>
+        <script src="dist/main.js"></script>
+    </body>
+</html>
